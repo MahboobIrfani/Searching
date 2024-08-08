49f6a999fd09651d7c1f30e06066d7f5dcefc637
MahboobIrfani <139584868+MahboobIrfani@users.noreply.github.com>
2024-08-09 00:23:36 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (52.52%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -10,31 +10,31 @@ class Solution {
             }
         }
         return count;*/
-       int rows = grid.length, cols = grid[0].length; 
-        int res = 0, lastNeg = cols - 1;
-        for (int row = 0; row < rows; row++) {
-            //check edge cases - if first element is < 0 - all elements in row are negative
-            if (grid[row][0] < 0) {
-                res+=cols;
+       int rows= grid.length;
+        int cols=grid[0].length;
+        int count=0;
+        int last=cols-1;
+        for(int i=0;i<rows;i++){
+            if(grid[i][0]<0){
+                count=count+cols;
                 continue;
             }
-            //if last element is positive - it means there are no negative numbers in a row
-            if (grid[row][cols - 1] > 0)
+            if(grid[i][cols-1]>0){
                 continue;
-            //there is a mix of negative and positive ones, need to find the border. starting
-            //binary search
-            int l = 0, r = lastNeg;
-            while (l <= r) {
-                int m = l + (r - l)/2;
-                if (grid[row][m] < 0) {
-                    r = m - 1;
-                } else
-                    l = m + 1;
             }
-            //l points to the first negative element, which means cols - l is a number of
-            //such elements
-            res += (cols - l); lastNeg = l;
+            int start=0;
+            int end=last;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(grid[i][mid]<0){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+            count=count+(cols-start);
+            last=start;
         }
-        return res;
+        return count;
         }
     }
